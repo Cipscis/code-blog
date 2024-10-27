9bcf44e3430d1e20f80ed336402baf496e9699e2
Brooke Hart <brooke+github@curly.kiwi>
2024-10-27 13:14:43 +1300

    Added header links

--- scripts/build-config/main.ts
+++ scripts/build-config/main.ts
@@ -5,6 +5,7 @@ import { dist, src } from './paths.js';
 export const config: BuildOptions = {
 	entryPoints: [
 		`${src}/main.ts`,
+		`${src}/syntax-highlighting.ts`,
 	],
 	outdir: dist,
 	bundle: true,
